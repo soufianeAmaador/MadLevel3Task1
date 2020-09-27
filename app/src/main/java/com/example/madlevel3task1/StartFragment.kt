87ed6fd667101daf9c2e5e048d54ef273ed3ad30
S. Amaador <soufiane.amaador@hva.nl>
2020-09-28 00:18:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel3task1/StartFragment.kt
@@ -0,0 +1,39 @@
+package com.example.madlevel3task1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        btnStart.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_startFragment_to_ratingFragment
+            )
+        }
+    }
+}
